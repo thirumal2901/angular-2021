2531e4106fc1b6030c25e176e6c346f83c51b63d
thirumal2901 <thirumalm022@gmail.com>
2021-01-06 12:32:37 +0530

    2 files modified-add delete(splice array)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,12 @@ export class AppComponent {
     this.todoArray.push(value);
     console.log(this.todoArray);
   }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if ((todo = this.todoArray[i])) {
+        this.todoArray.splice(i,1);
+      }
+    }
+  }
 }
